025b7abdf4b9fc26116ab9bb7d6a5d9f62be151b
Joel Clyne <joel.clyne2@gmail.com>
2020-08-20 20:14:51 -0400

    added string resource file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
+import websiteContent from './siteContent'
 
 function App() {
   return (
     <div className="App">
-      <h1>Title</h1>
+      <h1>{websiteContent.Title}</h1>
       <iframe src="https://onedrive.live.com/embed?cid=81B4805C909EB4A1&amp;resid=81B4805C909EB4A1%212408&amp;authkey=AN2saMVYEbhlIu0&amp;em=2&amp;wdAr=1.7777777777777777" width="962px" height="565px" frameborder="0">This is an embedded <a target="_blank" href="https://office.com">Microsoft Office</a> presentation, powered by <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>
-      <h2>Description</h2>
-      <p>Information goes here</p>
-      <h2>Credits go at bottom</h2>
+      <h2>{websiteContent.Description_header}</h2>
+      <p>{websiteContent.Description_Body}</p>
+      <h2>{websiteContent.Credits}</h2>
     </div>
   );
 }
---
+++ src/siteContent.js
@@ -0,0 +1,8 @@
+const websiteContent = {
+    Title: "Insert TITLE here",
+    Description_header: "Insert DESCRIPTION-HEADER here",
+    Description_Body: "Insert DESCIRPTION-BODY here",
+    Credits: "Insert CREDITS here"
+}
+
+export default websiteContent
