93b49dc098ad27a8fce247658aaf613c80c6fa98
Joel Clyne <joel.clyne2@gmail.com>
2020-08-25 14:16:17 -0400

    Updates

---
+++ precache-manifest.e2851fc17aa646b9860d93f7799d9d5a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37618df709767c59b0cbf23653f046b1",
+    "url": "/escape2020/index.html"
+  },
+  {
+    "revision": "4d28d8d8533a5ebde0d0",
+    "url": "/escape2020/static/css/main.d4634ac0.chunk.css"
+  },
+  {
+    "revision": "5fa29f967b13d2daac70",
+    "url": "/escape2020/static/js/2.dc646e27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/escape2020/static/js/2.dc646e27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d28d8d8533a5ebde0d0",
+    "url": "/escape2020/static/js/main.8bc25e93.chunk.js"
+  },
+  {
+    "revision": "081d9de8fed12c25cbe3",
+    "url": "/escape2020/static/js/runtime-main.ce0b355d.js"
+  },
+  {
+    "revision": "b966ab709f1d44ea8515c4aded9557d4",
+    "url": "/escape2020/static/media/VIPER.b966ab70.TTF"
+  },
+  {
+    "revision": "a401c2d8d27c8ae75a4bdac900b1f388",
+    "url": "/escape2020/static/media/caution-642510.a401c2d8.jpg"
+  },
+  {
+    "revision": "91985fd3526e920117d96161b12cb940",
+    "url": "/escape2020/static/media/viper_nora.91985fd3.otf"
+  }
+]);
