1fea1674105b85a443e1b71eaf63676df4fe5106
Joel Clyne <joel.clyne2@gmail.com>
2020-08-25 14:06:33 -0400

    Added credits

--- src/App.js
+++ src/App.js
@@ -10,11 +10,24 @@ function App() {
     <img style={{maxWidth: "25vw", display: "block", minHeight:"100vh"}}src={require('./assets/caution-642510.jpg')}></img>
     <div style={{minWidth: '50vw', textAlign: "center", color:"#000000", fontFamily:"viper"}}>
       <h1>{websiteContent.Title}</h1>
-      <h2 style={{color: "red", fontSize: "20px"}}>{websiteContent.Disclaimer}</h2>
+      <h2 style={{color: "red", fontSize: "20px", fontFamily: "sans-serif"}}>{websiteContent.Disclaimer}</h2>
       <iframe src="https://onedrive.live.com/embed?cid=81B4805C909EB4A1&amp;resid=81B4805C909EB4A1%212408&amp;authkey=AN2saMVYEbhlIu0&amp;em=2&amp;wdAr=1.7777777777777777" width="962px" height="565px" frameborder="0">This is an embedded <a target="_blank" href="https://office.com">Microsoft Office</a> presentation, powered by <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>
       <h2>{websiteContent.Description_header}</h2>
       <p>{websiteContent.Description_Body}</p>
       <h2>{websiteContent.Credits}</h2>
+      <div style={{fontFamily: "sans-serif"}}>
+        <h3 style={{fontFamily: "viper"}}>{websiteContent.Credits1}</h3>
+        <p>{websiteContent.Credits2}</p>
+        <p>{websiteContent.Credits3}</p>
+        <h3 style={{fontFamily: "viper"}}>{websiteContent.Credits4}</h3>
+        <p>{websiteContent.Credits6}</p>
+        <p>{websiteContent.Credits7}</p>
+        <p>{websiteContent.Credits3}</p>
+        <h3 style={{fontFamily: "viper"}}>{websiteContent.Credits5}</h3>
+        <p>{websiteContent.Credits8}</p>
+        <p>{websiteContent.Credits9}</p>
+      </div>
+      
     </div>
     <img style={{maxWidth: "25vw", display: "block", minHeight:"100vh"}}src={require('./assets/caution-642510.jpg')}></img>
   </div>
--- src/siteContent.js
+++ src/siteContent.js
@@ -1,9 +1,18 @@
 const websiteContent = {
-    Title: "Insert TITLE here",
-    Description_header: "Insert DESCRIPTION-HEADER here",
+    Title: "ESCAPE: 2020",
+    Description_header: "About the game",
     Description_Body: "Insert DESCIRPTION-BODY here",
-    Credits: "Insert CREDITS here",
-    Disclaimer: "Insert DISCLAIMER here"
+    Credits: "Credits",
+    Disclaimer: "This game contains information about sensitive events. Viewer discretion is advised.",
+    Credits1: "Website Developers",
+    Credits2: "Joel Clyne",
+    Credits3: "Diana Diakete",
+    Credits4: "Graphic Designers", 
+    Credits5: "Concept Designers",
+    Credits6: "Tongxin Chen",
+    Credits7: "Grace Lin",
+    Credits8: "Sophia Chen",
+    Credits9: "Dechen Rota"
 }
 
 export default websiteContent
